74c0ffe25e9c7336afa554afee917c6d81617db6
Joeri Verdeyen <joeri.verdeyen@kunstmaan.be>
2013-06-06 15:13:40 +0200

    Remove config.yml and add default values to configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -22,6 +22,7 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->children()
                 ->arrayNode('link_page_part')
+                    ->addDefaultsIfNotSet()
                     ->children()
                         ->scalarNode('devitize_filter')
                             ->defaultValue(false)
